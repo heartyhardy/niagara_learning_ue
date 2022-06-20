d1ac152f5af07ba26aeb45b20ee44b6d0a8fff56
heartyhardy <charith.n.rathnayake@gmail.com>
2022-06-21 01:08:40 +0530

    Niagara Day 1

---
+++ Source/NiagaraTutorials/NiagaraTutorials.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NiagaraTutorials.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NiagaraTutorials, "NiagaraTutorials" );
+ 
---
+++ Source/NiagaraTutorials/NiagaraTutorialsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NiagaraTutorialsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ANiagaraTutorialsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ANiagaraTutorialsGameMode();
+};
+
+
+
